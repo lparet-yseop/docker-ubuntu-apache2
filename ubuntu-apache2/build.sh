df233277890e1870cbca3c452d802d81e5bbbddc
lparet <lparet@yseop.com>
2017-12-05 12:12:48 +0100

    Add push.sh, without build cache, change directory name

--- ubuntu-apache2/build.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-set -e
-
-source version.sh
-
-echo "Execute : docker build -t ${REGISTRY}/${IMAGE} . "
-docker build -t ${REGISTRY}/${IMAGE} .
-
-echo "Push : docker push ${REGISTRY}/${IMAGE}"
